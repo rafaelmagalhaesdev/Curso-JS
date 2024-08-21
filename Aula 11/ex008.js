3e7c779264649ebcf17a7d8ae4eff333b99e6c48
Rafael Gabriel Magalhães <devrafaelmagalhaes@hotmail.com>
2024-08-21 06:20:05 -0300

    Aula de Condições
    Condições Simples e Condições Compostas

---
+++ Aula 11/ex008.js
@@ -0,0 +1,7 @@
+console.log('O console funcinou corretamente')
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if(vel > 60) { // Condição Multa
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
