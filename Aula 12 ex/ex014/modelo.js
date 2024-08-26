086e69541852a279bd1f6254a8e1929a45e8dc69
Rafael Gabriel Magalhães <devrafaelmagalhaes@hotmail.com>
2024-08-26 05:39:38 -0300

    Finalização exercicios de HTML/CSS/JS
    Aprendizado: HTML estrutural, CSS básico, JS básico com Function (if and else)

--- Aula 12 ex/ex014/modelo.js
+++ Aula 12 ex/ex014/modelo.js
@@ -1,17 +1,20 @@
 function carregar(){
     var msg = document.getElementById('msg');
     var img = document.getElementById('imagem');
-    var data = new Date();
-    var hora = data.getHours();
-    msg.innerHTML = `Agora são ${hora} horas.`;
+    var data = new Date(); // Aqui pega a data de hoje
+    var hora = data.getHours(); // Aqui pega a hora atual 
+    msg.innerHTML = `Agora são ${hora} horas.`; // Aqui mostra a msg na tela
     if (hora >= 0 && hora < 12){
         // Bom dia
         img.src = 'manha.jpg';
+        document.body.style.background = '#C2CCCE'
     } else if (hora >= 12 && hora < 18){
         // Boa tarde
         img.src = 'tarde.jpg';
+        document.body.style.background = '#FFC756'
     } else {
         // Boa noite
         img.src = 'noite.jpg';
+        document.body.style.background = '#2E2E35'
     }
 }
