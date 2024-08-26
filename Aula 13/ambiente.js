f1d9b74dfa95a955bbd0d425e32dde453352d7a1
Rafael Gabriel Magalhães <devrafaelmagalhaes@hotmail.com>
2024-08-26 19:53:45 -0300

    Primeiras impressões sobre "While e Do"
    Estuturas de repetição

---
+++ Aula 13/ambiente.js
@@ -0,0 +1,26 @@
+var c = 1
+do {
+    console.log(`Paso ${c}`)
+    c++
+} while (c <= 6)
+
+
+
+// Condição de repetição no inicio: 
+
+/* var c = 1 
+while (c <= 6){
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+// Pior condição de repetição: 
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
