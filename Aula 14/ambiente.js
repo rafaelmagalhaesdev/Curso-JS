5429de28ecb78a2b13fc18960c6956f86b4adbbb
Rafael Gabriel Magalhães <devrafaelmagalhaes@hotmail.com>
2024-08-27 05:59:52 -0300

    Aprendendo "for"
    Usando o "for" para repetição

---
+++ Aula 14/ambiente.js
@@ -0,0 +1,26 @@
+console.log('Vai começar ...')
+for (var c = 1; c <= 5; c++){
+    console.log(c)
+}
+console.log('FIM!')
+
+
+/*
+
+Usando for 
+
+for(var c = 1;c <= 5;c++){
+    console.log(c)
+}
+
+*/
+
+
+/* 
+usando while
+var c = 1 
+while (c <= 5){
+    console.log(c)
+    c++
+}
+*/ 
