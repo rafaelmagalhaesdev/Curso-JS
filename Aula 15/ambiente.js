56e02a709bec5cc12f4a01b12f902d24d88a249a
Rafael Gabriel Magalhães <devrafaelmagalhaes@hotmail.com>
2024-08-29 06:12:05 -0300

    Funções

--- Aula 15/ambiente.js
+++ Aula 15/ambiente.js
@@ -2,7 +2,7 @@ let num = [6,5,4,3,2,1]
 num.sort()
 console.log(num)
 console.log(`O vetor tem ${num.length} posições`)
-console.log(`O primeiro valor do vetor é: ${num[3]}`)
+console.log(`O primeiro valor do vetor é: ${num[1]}`)
 let pos = num.indexOf(2)
 if (pos == -1){
     console.log('O valor não foi encontrado')
---
+++ Aula 16/função02.js
@@ -0,0 +1,4 @@
+function soma(n1=0, n2=0){ //para que a não haja erro podemos fazer o "n1" considerar "0" colocando o n1=0 assim não tornando o valor NaN
+    return n1 + n2
+}
+console.log(soma(2,8)) //Quando não colocamos um valor da um "erro" e com isso considerei colocar um "n1=0" para que isso não ocorra
